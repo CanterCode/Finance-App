263df396b0d9251618cb12a5a4e00a12d6de49bc
Josh Canterbury <joshcanterburyofficial@gmail.com>
2025-12-10 22:23:17 -0600

    home page finished

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello Expo.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Money Mate!</Text>
+      <Text style={styles.subTitle}>
+        Your friendly finance helper — calculate loans, split tips, plan your
+        budget, and more.
+      </Text>
     </View>
-    // <div>
-    //   <p>Hello World</p>
-    // </div>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+    backgroundColor: "#25292e",
+  },
+  title: {
+    color: "white",
+    fontSize: 28,
+    fontWeight: "600",
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  subTitle: {
+    color: "rgba(255, 255, 255, 0.85)",
+    fontSize: 24,
+    lineHeight: 26,
+    textAlign: "center",
+  },
+});
